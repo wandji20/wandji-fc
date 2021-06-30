4f43cca32f200fc192a2559ad52be22fc6068fc1
wandji20 <wbertrand10@gmail.com>
2021-06-30 20:25:41 +0100

    Make nav links clickable

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,38 @@
 /* eslint-disable */
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Nav = () => (
   <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
-
-    <h3 className='home'>Limbe FC</h3>
+    <Link to ='/' className="nav-link">        
+      <h3 className='home'>Limbe FC</h3>
+    </Link>
     <ul className="navbar-nav  d-flex justify-content-around w-75">
-      <li className="nav-item">
-        Players
-      </li>
-      <li className="nav-item">
-        Fixtures
-      </li>
-      <li className="nav-item">
-        Trophies
-      </li>
-      <li className="nav-item">
-        About
-      </li>
-      <li className="nav-item">
-        User Icon
-      </li>
+      <Link to ='/players' className="nav-link">
+        <li className="nav-item">
+          Players
+        </li>
+      </Link>
+      <Link to ='/fixtures' className="nav-link">
+        <li className="nav-item">
+          Fixtures
+        </li>
+      </Link>
+      <Link to ='/trophies' className="nav-link">
+        <li className="nav-item">
+          Trophies
+        </li>
+      </Link>
+      <Link to ='/about' className="nav-link">        
+        <li className="nav-item">
+          About
+        </li>
+      </Link>
+      <Link to='/admin' className="nav-link">        
+        <li className="nav-item">
+          User Icon
+        </li>
+      </Link>
     </ul>
 
   </nav>
